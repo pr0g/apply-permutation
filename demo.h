bdd8c35b3f9294ca00ac28d121623811dd1d7436
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2026-01-03 20:30:19 +0000

    add apply_permutation and apply_inverse_permutation implementation

---
+++ demo.h
@@ -0,0 +1,46 @@
+#pragma once
+
+template<typename Indices, typename... Objects>
+  requires std::random_access_iterator<std::remove_reference_t<Indices>>
+        || std::is_pointer_v<std::remove_reference_t<Indices>>
+void apply_permutation(
+  const size_t begin, const size_t end, Indices indices, Objects... objects) {
+  using std::swap;
+  for (size_t i = begin; i < end; i++) {
+    auto current = i;
+    // while there's a cycle (‘indices[current]’ doesn’t point to ‘i’)
+    while (i != indices[current - begin]) {
+      // store the index which should be in the current position
+      const auto next = indices[current - begin];
+      // swap ‘next’ with ‘current’ – ‘next’ is now where it should be
+      ([&](const auto it) { swap(it[current], it[next]); }(objects), ...);
+      // write ‘current’ to current position in indices (current position and
+      // ‘indices[current]’ match)
+      indices[current - begin] = current;
+      // move to next index in the cycle
+      current = next;
+    }
+    // when cycle is reached, write ‘current’ to current position in indices
+    // (ensures earlier swapped value has the correct index in indices)
+    indices[current - begin] = current;
+  }
+}
+
+template<typename Indices, typename... Objects>
+  requires std::random_access_iterator<std::remove_reference_t<Indices>>
+        || std::is_pointer_v<std::remove_reference_t<Indices>>
+void apply_inverse_permutation(
+  const size_t begin, const size_t end, Indices indices, Objects... objects) {
+  using std::swap;
+  for (size_t i = begin; i < end; i++) {
+    // while 'indices[i]' is not in the correct position (matches its offset)
+    while (i != indices[i - begin]) {
+      // look-up where the current index/object should be
+      const auto next = indices[i - begin];
+      // swap the object/index to move them to the correct final position
+      // move what was in that slot to the current position
+      ([&](const auto it) { swap(it[i], it[next]); }(objects), ...);
+      swap(indices[i - begin], indices[next - begin]);
+    }
+  }
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,70 @@
+#include <algorithm>
 #include <iostream>
+#include <random>
+#include <vector>
+
+#include "demo.h"
+
+void demo_apply_permutation() {
+  std::vector<int32_t> objects(10);
+  std::iota(objects.begin(), objects.end(), 1);
+
+  const size_t seed = 1;
+  std::mt19937 gen(seed);
+  std::shuffle(objects.begin(), objects.end(), gen);
+  std::println("{}", objects);
+
+  std::vector<int32_t> indices(objects.size());
+  std::iota(indices.begin(), indices.end(), 0);
+  std::sort(indices.begin(), indices.end(), [&objects](auto lhs, auto rhs) {
+    return objects[lhs] < objects[rhs];
+  });
+
+  apply_permutation(0, objects.size(), indices.begin(), objects.begin());
+  std::println("{}", objects);
+}
+
+void demo_apply_inverse_permutation() {
+  std::vector<int32_t> objects(10);
+  std::iota(objects.begin(), objects.end(), 1);
+
+  const size_t seed = 1;
+  std::mt19937 gen(seed);
+  std::shuffle(objects.begin(), objects.end(), gen);
+  std::println("{}", objects);
+
+  const int32_t offset = 3;
+  const int32_t range = 5;
+
+  std::vector<int32_t> indices(range);
+  std::iota(indices.begin(), indices.end(), offset);
+  std::shuffle(indices.begin(), indices.end(), gen);
+  std::sort(indices.begin(), indices.end(), [&objects](auto lhs, auto rhs) {
+    return objects[lhs] < objects[rhs];
+  });
+
+  std::vector<int32_t> result_1(objects.size());
+  for (int32_t i = offset; i < offset + range; i++) {
+    result_1[i] = objects[indices[i - offset]];
+  }
+
+  std::println("{}", result_1);
+
+  std::vector<int32_t> result_2(objects.size());
+  for (int32_t i = offset; i < offset + range; i++) {
+    result_2[indices[i - offset]] = result_1[i];
+  }
+
+  apply_inverse_permutation(
+    offset, offset + range, indices.begin(), result_1.begin());
+
+  std::println("{} - copy", result_2);
+  std::println("{} - inplace", result_1);
+}
 
 int main(int argc, char** argv) {
-  std::println("{}", "hello, world");
+  demo_apply_permutation();
+  std::println("---");
+  demo_apply_inverse_permutation();
   return 0;
 }
