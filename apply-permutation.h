27313c10c611766fc6f609c36e24b2001b793bd1
Tom Hulton-Harrop <tom.hultonharrop@gmail.com>
2026-01-10 14:50:54 +0000

    improve comment slightly for apply permutation functions

--- apply-permutation.h
+++ apply-permutation.h
@@ -8,7 +8,8 @@ void apply_permutation(
   using std::swap;
   for (size_t i = begin; i < end; i++) {
     auto current = i;
-    // while there's a cycle (‘indices[current]’ doesn’t point to ‘i’)
+    // while there's a cycle (‘indices[current]’ doesn’t point to ‘i’ - its
+    // current offset/position)
     while (i != indices[current - begin]) {
       // store the index which should be in the current position
       const auto next = indices[current - begin];
@@ -33,7 +34,7 @@ void apply_inverse_permutation(
   const size_t begin, const size_t end, Indices indices, Objects... objects) {
   using std::swap;
   for (size_t i = begin; i < end; i++) {
-    // while 'indices[i]' is not in the correct position (matches its offset)
+    // while 'indices[i]' is not in the correct position/matches its offset
     while (i != indices[i - begin]) {
       // look-up where the current index/object should be
       const auto next = indices[i - begin];
